e627b777c971563c71d56ce1d614ea314af5c624
Velgrab <velgrab@gmail.com>
2022-08-08 12:11:37 -0400

    Changes for Deploy

--- api/src/routes/Controllers.js
+++ api/src/routes/Controllers.js
@@ -4,6 +4,7 @@ const { API_KEY } = process.env;
 const { Videogame, Genre } = require("../db");
 
 const getAllGenres = async () => {
+  try {
     const genresApi = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`);
     genresApi.data.results.map((e) => {
       Genre.findOrCreate({
@@ -14,6 +15,10 @@ const getAllGenres = async () => {
     })
     const genresDb = await Genre.findAll();
     return genresDb;
+  } catch (error) {
+    console.log(error);
+  }
+    
 }
 
 module.exports = {
