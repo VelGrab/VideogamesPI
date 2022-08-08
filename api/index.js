2e6a0fb658f62d0880b0fcae5ffddaea6b51c3e3
Velgrab <velgrab@gmail.com>
2022-08-07 22:54:02 -0400

    Changes for Deploy

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ getAllGenres()
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
