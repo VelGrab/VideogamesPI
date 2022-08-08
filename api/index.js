497d6f78d18124100f71cae7ed516835eb4c681e
Velgrab <velgrab@gmail.com>
2022-08-08 11:15:53 -0400

    Changes for Deploy

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { getAllGenres } = require('./src/routes/controllers')
+const { getAllGenres } = require('./src/routes/controllers.js')
 getAllGenres()
 
 // Syncing all the models at once.
