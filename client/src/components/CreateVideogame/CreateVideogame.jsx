799041505ebd622cf3ce63ebc6928afcb1fa330c
Velgrab <velgrab@gmail.com>
2022-08-12 22:07:45 -0400

    Changes for deploy

--- client/src/components/CreateVideogame/CreateVideogame.jsx
+++ client/src/components/CreateVideogame/CreateVideogame.jsx
@@ -36,13 +36,13 @@ const validateForm = (input) => {
   if (!input.released) {
     errors.released = "Se requiere una fecha de lanzamiento";
   } else if (
-    /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/.test(
+    /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/.test(
       input.released
     )
   ) {
     errors.released = "La fecha de lanzamiento debe ser una fecha valida";
   }
-  if (input.image === "") {
+  if (!input.image) {
   } else if (
     !/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#()?&//=]*)/.test(
       input.image
@@ -183,6 +183,7 @@ export default function CreateVideogame() {
               type="text"
               value={input.name}
               name="name"
+              required={true}
             ></input>
             {errors.name && <p className={style.errorText}>{errors.name}</p>}
           </div>
